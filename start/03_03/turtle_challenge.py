482606b897efe7f55f67c57bce200df41857192a
Ruben Hernandez <166978446+rubenjh2@users.noreply.github.com>
2025-06-10 23:47:58 -0500

    6/10

--- start/03_03/turtle_challenge.py
+++ start/03_03/turtle_challenge.py
@@ -2,4 +2,40 @@
 Chapter 3 challenge.
 The world is your Turtle! 
 """
-import turtle
+import turtle as t
+import numpy as np
+
+# choose the number of racers
+players = 5
+
+# setup screen
+screen = t.Screen()
+screen.setup(0.5, 0.5, None, None)
+screen.title("Turtle Race")
+
+
+turtles = []
+# create turtle object for each player
+for i in range(players):
+    turtle = t.Turtle()
+    turtles.append(turtle)
+
+y_pos = np.linspace(-200, 200, players)
+
+# allow for HSL coloring
+turtle.colormode(255)
+
+
+for j in range(players):
+    turtles[j].teleport(-450, y_pos[j])
+
+    # setting unique color per turtle
+    turtles[j].color("blue")
+    print(turtles[j].position())
+
+
+# # turtle1.fd(-250)
+# # print(turtle1.position())
+
+
+t.done()
