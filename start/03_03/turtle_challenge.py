f392ab82220cf641e14409321aa1bdf51bf06bac
Ruben Hernandez <166978446+rubenjh2@users.noreply.github.com>
2025-06-13 22:38:13 -0500

    6/13

--- start/03_03/turtle_challenge.py
+++ start/03_03/turtle_challenge.py
@@ -4,38 +4,49 @@ The world is your Turtle!
 """
 import turtle as t
 import numpy as np
+import colorsys
+import random
 
 # choose the number of racers
-players = 5
+players = 10
 
 # setup screen
 screen = t.Screen()
 screen.setup(0.5, 0.5, None, None)
 screen.title("Turtle Race")
 
-
 turtles = []
-# create turtle object for each player
-for i in range(players):
-    turtle = t.Turtle()
-    turtles.append(turtle)
-
-y_pos = np.linspace(-200, 200, players)
 
 # allow for HSL coloring
-turtle.colormode(255)
-# CONTINUE WITH SETTING UNIQUE COLORS PER PLAYER
+t.colormode(255)
 
-for j in range(players):
-    turtles[j].teleport(-450, y_pos[j])
+# get equally spaced starting locations
+y_pos = np.linspace(-200, 200, players)
+
+# setup for race
+for i in range(players):
+    turtle = t.Turtle()
+    # move to starting location
+    turtle.teleport(-450, y_pos[i])
+
+    turtle.shape("turtle")
+    # Hide trail
+    turtle.penup()
+    turtle.speed(20)
 
     # setting unique color per turtle
-    turtles[j].color("blue")
-    print(turtles[j].position())
+    hue = i / players
+    r, g, b = [int(255 * c) for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+    turtle.color((r, g, b))
 
+    turtles.append(turtle)
 
-# # turtle1.fd(-250)
-# # print(turtle1.position())
+# if any turtle hits x_pos = 430, stop the race
+while all(player.position()[0] < 430 for player in turtles):
+
+    for j in range(players):
+        move = random.randint(0, 40)
+        turtles[j].fd(move)
 
 
 t.done()
